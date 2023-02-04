244fb50a932b676730f5eb118d71e07b0c90731f
Venkateshpasam <112842211+Venkateshpasam@users.noreply.github.com>
2023-02-04 14:00:48 +0530

    Create bfs.c

---
+++ bfs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int adj[MAX][MAX], visited[MAX], n;
+void BFS(int v) {
+    int i;
+    visited[v] = 1;
+    printf("%d ", v);
+    for (i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0)
+            BFS(i);
+    }
+}
+int main() {
+    int i, j, v;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+    printf("Enter the starting vertex: ");
+    scanf("%d", &v);
+    BFS(v);
+    return 0;
+}
